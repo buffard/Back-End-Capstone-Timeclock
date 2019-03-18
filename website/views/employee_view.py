2abd0bb87dfaa71396420f33ba94664d7c04188e
buffard <samuelwebber19@gmail.com>
2019-03-18 14:13:38 -0500

    now have working login/auth for employees

--- website/views/employee_view.py
+++ website/views/employee_view.py
@@ -1,11 +1,20 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from ..models import Employee
 
 
+@login_required
 def employeeList(request):
-    """ returns a lists of all employees in the Employees table """
-    all_employees = Employee.objects.all()
+  """ returns a lists of all employees in the Employees table """
+  if request.method == "GET":
+    current_user = request.user
+    all_employees = Employee.objects.filter(manager_id=current_user.id)
     context = { 'all_employees' : all_employees }
     return render(request, 'website/employee_list.html', context)
+
+
+
+     
--- website/views/userHome_view.py
+++ website/views/userHome_view.py
@@ -1,10 +1,14 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from ..models import Employee, Shift
 
 
-
+@login_required
 def userHome(request):
-    all_employees = Employee.objects.all()
+  if request.method == "GET":
+    current_user = request.user
+    all_employees = Employee.objects.filter(manager_id=current_user.id)
     context = {'all_employees': all_employees}
     return render(request, 'website/user_home.html', context)
